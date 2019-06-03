7c536f0153aec646c856e91ed1952b614d2db1e1
unknown <jean.miraval@utec.edu.pe>
2019-06-03 16:09:12 -0500

    Ahora si xd

--- web/model/entities.py
+++ web/model/entities.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from database import connector
 
+
 class User(connector.Manager.Base):
     __tablename__ = 'users'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
@@ -9,13 +10,12 @@ class User(connector.Manager.Base):
     fullname = Column(String(50))
     password = Column(String(12))
     username = Column(String(12))
+    #type = Column(String(12)) - Esto debería ir para asignar un tipo de user (cliente, admin o repartidor)
 
-class Message(connector.Manager.Base):
-    __tablename__ = 'messages'
-    id = Column(Integer, Sequence('message_id_seq'), primary_key=True)
-    content = Column(String(500))
-    sent_on = Column(DateTime(timezone=True))
-    user_from_id = Column(Integer, ForeignKey('users.id'))
-    user_to_id = Column(Integer, ForeignKey('users.id'))
-    user_from = relationship(User, foreign_keys=[user_from_id])
-    user_to = relationship(User, foreign_keys=[user_to_id])
+class Pedido(connector.Manager.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key = True)
+    pedido = Column(String(50))
+    direccion = Column(String(50))
+    fecha = Column(String(12))
+    estado = Column(String(12)) #Pendiente o realizado
