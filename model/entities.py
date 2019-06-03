6c9e1839baa65134f2897c894f4d798198475008
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2019-06-03 14:13:57 -0500

    Update entities.py

--- model/entities.py
+++ model/entities.py
@@ -10,4 +10,12 @@ class User(connector.Manager.Base):
     fullname = Column(String(50))
     password = Column(String(12))
     username = Column(String(12))
+    #type = Column(String(12)) - Esto debería ir para asignar un tipo de user (cliente, admin o repartidor)
 
+class Pedido(connector.Manager.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key = True)
+    pedido = Column(String(50))
+    direccion = Column(String(50))
+    fecha = Column(String(12))
+    estado = Column(String(12)) #Pendiente o realizado
