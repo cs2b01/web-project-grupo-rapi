a77c75a52e71493c87c7b50a6f25bdf1937051d0
unknown <jean.miraval@utec.edu.pe>
2019-06-03 16:04:39 -0500

    He implementado el login

--- web/server.py
+++ web/server.py
@@ -8,6 +8,8 @@ engine = db.createEngine()
 
 app = Flask(__name__)
 
+app.debug=True
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,6 +17,27 @@ def index():
 @app.route('/static/<content>')
 def static_content(content):
     return render_template(content)
+
+@app.route('/authenticate', methods = ['POST'])
+def authenticate():
+    message = json.loads(request.data)
+    username = message['username']
+    password = message['password']
+
+
+    db_session = db.getSession(engine)
+
+    try:
+        user = db_session.query(entities.User
+            ).filter(entities.User.username == username
+            ).filter(entities.User.password == password
+            ).one()
+        message = {'message' : 'Authorized'}
+        return Response(message, status=200, mimetype='application/json')
+    except Exception:
+        message = {'message' : 'Unauthorized'}
+        return Response(message, status=401, mimetype='application/json')
+
 
 
 if __name__ == '__main__':
